be3b6e9568e3b3efb38f8cf22c0a7573fdc8f470
WasTabon <72084063+WasTabon@users.noreply.github.com>
2026-07-01 01:39:14 +0200

    ui

--- Assets/SwimGame/Scripts/UI/ShopPopup.cs
+++ Assets/SwimGame/Scripts/UI/ShopPopup.cs
@@ -18,6 +18,10 @@ public class ShopPopup : PopupBase
     [SerializeField] private TextMeshProUGUI shieldCountText;
     [SerializeField] private Button closeButton;
 
+    private GameObject noMoneyToast;
+    private RectTransform noMoneyToastRect;
+    private Sequence noMoneyToastSeq;
+
     private void Awake()
     {
         flippersBuyButton.onClick.AddListener(() => Buy(ItemType.Flippers, FlippersPrice));
@@ -37,6 +41,13 @@ public class ShopPopup : PopupBase
     {
         CurrencyManager.OnBalanceChanged -= HandleBalanceChanged;
         ItemInventory.OnChanged -= Refresh;
+        noMoneyToastSeq?.Kill();
+        if (noMoneyToast != null)
+        {
+            Destroy(noMoneyToast);
+            noMoneyToast = null;
+            noMoneyToastRect = null;
+        }
     }
 
     private void HandleBalanceChanged(int balance)
@@ -69,10 +80,55 @@ public class ShopPopup : PopupBase
             SoundManager.Instance.PlaySfx(SfxType.Deny);
             balanceText.transform.DOKill(true);
             balanceText.transform.DOPunchPosition(new Vector3(12f, 0f, 0f), 0.3f, 12, 0.6f);
+            ShowNoMoneyToast();
             return;
         }
         ItemInventory.Add(type, 1);
         SoundManager.Instance.PlaySfx(SfxType.Coin);
         HapticManager.Instance.Vibrate();
+    }
+
+    private void ShowNoMoneyToast()
+    {
+        if (noMoneyToast == null)
+        {
+            var canvas = GetComponentInParent<Canvas>();
+
+            noMoneyToast = new GameObject("NoMoneyToast", typeof(RectTransform));
+            noMoneyToast.transform.SetParent(canvas.transform, false);
+            noMoneyToastRect = noMoneyToast.GetComponent<RectTransform>();
+            noMoneyToastRect.anchorMin = new Vector2(0.5f, 0.5f);
+            noMoneyToastRect.anchorMax = new Vector2(0.5f, 0.5f);
+            noMoneyToastRect.pivot = new Vector2(0.5f, 0.5f);
+            noMoneyToastRect.anchoredPosition = new Vector2(0f, 360f);
+            noMoneyToastRect.sizeDelta = new Vector2(660f, 150f);
+
+            var bg = noMoneyToast.AddComponent<Image>();
+            bg.color = new Color32(196, 60, 60, 240);
+            bg.raycastTarget = false;
+
+            var textGo = new GameObject("Text", typeof(RectTransform));
+            textGo.transform.SetParent(noMoneyToast.transform, false);
+            var textRect = textGo.GetComponent<RectTransform>();
+            textRect.anchorMin = Vector2.zero;
+            textRect.anchorMax = Vector2.one;
+            textRect.offsetMin = new Vector2(20f, 0f);
+            textRect.offsetMax = new Vector2(-20f, 0f);
+            var text = textGo.AddComponent<TextMeshProUGUI>();
+            text.text = "Not enough coins!";
+            text.fontSize = 48f;
+            text.fontStyle = FontStyles.Bold;
+            text.alignment = TextAlignmentOptions.Center;
+            text.color = Color.white;
+            text.raycastTarget = false;
+        }
+
+        noMoneyToast.transform.SetAsLastSibling();
+        noMoneyToastSeq?.Kill();
+        noMoneyToastRect.localScale = Vector3.zero;
+        noMoneyToastSeq = DOTween.Sequence();
+        noMoneyToastSeq.Append(noMoneyToastRect.DOScale(1f, 0.3f).SetEase(Ease.OutBack));
+        noMoneyToastSeq.AppendInterval(0.9f);
+        noMoneyToastSeq.Append(noMoneyToastRect.DOScale(0f, 0.25f).SetEase(Ease.InBack));
     }
 }
